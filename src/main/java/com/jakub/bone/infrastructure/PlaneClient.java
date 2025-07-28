1597061b04fe9828805db29ceec0f1de2c855a5f
Cezary Sanecki <saneckicezary@gmail.com>
2025-07-28 16:35:47 +0200

    Inlined methods

--- src/main/java/com/jakub/bone/infrastructure/PlaneClient.java
+++ src/main/java/com/jakub/bone/infrastructure/PlaneClient.java
@@ -11,6 +11,8 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.ThreadContext;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 @Log4j2
 @Getter
@@ -18,8 +20,6 @@ public class PlaneClient implements Runnable {
 
     private final SocketClient socketClient;
     private final Plane plane;
-    private PlaneInstructionHandler instructionHandler;
-    private PlaneCommunicationService communicationService;
 
     public PlaneClient(String ip, int port, Plane plane) {
         this.socketClient = new SocketClient(ip, port);
@@ -36,44 +36,38 @@ public class PlaneClient implements Runnable {
     private void connectAndHandle() {
         ThreadContext.put("type", "Client");
         try {
-            establishConnection();
-            initializeServices();
+            socketClient.startConnection();
+            log.info("PlaneClient [{}]: connected to server", plane.getFlightNumber());
+
+            ObjectInputStream in = socketClient.getIn();
+            ObjectOutputStream out = socketClient.getOut();
+
+            PlaneCommunicationService communicationService = new PlaneCommunicationService(out);
+            PlaneInstructionHandler instructionHandler = new PlaneInstructionHandler(plane, in, out);
+
             communicationService.sendInitialData(plane);
-            processInstructions();
+
+            while (!instructionHandler.isProcessCompleted()) {
+                communicationService.sendFuelLevel(plane.getFuelLevel());
+
+                if (plane.isOutOfFuel() || plane.getCoordinates() == null) {
+                    log.error("Plane [{}]: lost communication due to fuel or location issues", plane.getFlightNumber());
+                    break;
+                }
+                communicationService.sendLocation(plane.getCoordinates());
+
+                PlaneHandler.AirportInstruction instruction = Messenger.handleResponseAirportInstruction(in);
+                instructionHandler.processInstruction(instruction);
+
+                if (plane.isDestroyed()) {
+                    log.info("Plane [{}]: has destroyed", plane.getFlightNumber());
+                    break;
+                }
+            }
         } catch (IOException | ClassNotFoundException ex) {
             log.error("PlaneClient [{}]: encountered an error: {}", plane.getFlightNumber(), ex.getMessage(), ex);
         } finally {
             closeConnection();
-        }
-    }
-
-    private void establishConnection() throws IOException {
-        socketClient.startConnection();
-        log.info("PlaneClient [{}]: connected to server", plane.getFlightNumber());
-    }
-
-    private void initializeServices() {
-        this.communicationService = new PlaneCommunicationService(socketClient.getOut());
-        this.instructionHandler = new PlaneInstructionHandler(plane, socketClient.getIn(), socketClient.getOut());
-    }
-
-    private void processInstructions() throws IOException, ClassNotFoundException {
-        while (!instructionHandler.isProcessCompleted()) {
-            communicationService.sendFuelLevel(plane.getFuelLevel());
-
-            if (plane.isOutOfFuel() || plane.getCoordinates() == null) {
-                log.error("Plane [{}]: lost communication due to fuel or location issues", plane.getFlightNumber());
-                return;
-            }
-            communicationService.sendLocation(plane.getCoordinates());
-
-            PlaneHandler.AirportInstruction instruction = Messenger.handleResponseAirportInstruction(socketClient.getIn());
-            instructionHandler.processInstruction(instruction);
-
-            if (plane.isDestroyed()) {
-                log.info("Plane [{}]: has destroyed", plane.getFlightNumber());
-                return;
-            }
         }
     }
 
