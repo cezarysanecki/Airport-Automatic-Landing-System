0759af054dfde4dbedefc433cfaaa6561b8e0b59
Cezary Sanecki <saneckicezary@gmail.com>
2025-08-04 21:51:41 +0200

    Changed naming

--- src/main/java/com/jakub/bone/service/PlanesRadarDbDump.java
+++ src/main/java/com/jakub/bone/service/PlanesRadarDbDump.java
@@ -1,30 +1,19 @@
 package com.jakub.bone.service;
 
-import com.jakub.bone.domain.airport.Runway;
 import com.jakub.bone.domain.plane.Plane;
 import com.jakub.bone.repository.CollisionRepository;
 import com.jakub.bone.repository.PlaneRepository;
-import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Supplier;
-
-import static com.jakub.bone.config.Constant.HOLDING_ENTRY_ALTITUDE;
-import static com.jakub.bone.config.Constant.MAX_CAPACITY;
 
 @Log4j2
-public class ControlTowerServiceDump {
+public class PlanesRadarDbDump {
 
     private final PlaneRepository planeRepository;
     private final CollisionRepository collisionRepository;
 
-    public ControlTowerServiceDump(PlaneRepository planeRepository, CollisionRepository collisionRepository) {
+    public PlanesRadarDbDump(PlaneRepository planeRepository, CollisionRepository collisionRepository) {
         this.planeRepository = planeRepository;
         this.collisionRepository = collisionRepository;
     }
